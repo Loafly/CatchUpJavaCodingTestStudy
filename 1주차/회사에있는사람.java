b3bb1a25891de666c84079b7ced02e360182aff8
NamSeok_DT <skatjr30@naver.com>
2021-07-02 19:55:52 +0900

    회사에있는사람

---
+++ 1주차/회사에있는사람.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.TreeSet;
+
+
+public class 회사에있는사람 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int length = scanner.nextInt();
+        scanner.nextLine();
+
+        TreeSet<String> enterList = new TreeSet<>();
+
+        for (int i = 0; i < length; i++){
+            String[] string = scanner.nextLine().split(" ");
+
+            if (string[1].equals("enter")){
+                enterList.add(string[0]);
+            }
+            // else if (string[1].equals("leave")){
+            else {
+                enterList.remove(string[0]);
+            }
+        }
+
+        // Collections.sort(enterList, Collections.reverseOrder());
+
+        for(String enter : enterList.descendingSet())
+        {
+            System.out.println(enter);
+        }
+
+    }
+    
+}
