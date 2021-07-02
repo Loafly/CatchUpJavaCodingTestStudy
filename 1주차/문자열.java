86d342363cc545558fb5b987c51e97fcffa14f69
NamSeok_DT <skatjr30@naver.com>
2021-07-02 14:33:53 +0900

    문자열 완료

--- 1주차/문자열.java
+++ 1주차/문자열.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class 문자열 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // 
+        Scanner scanner = new Scanner(System.in);
+        
+        int length = scanner.nextInt();
+
+        for (int i = 0; i < length ; i++){
+            String string = new String();
+            string = scanner.next();
+            
+            char start = string.charAt(0);
+            char end = string.charAt(string.length() - 1);
+
+            System.out.println(Character.toString(start) + Character.toString(end));
+        }
+
+        scanner.close();
+
     }
 }
