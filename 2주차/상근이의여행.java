001b75f5ea92a5e73f746d5aba34959597c90f8a
NamSeok_DT <skatjr30@naver.com>
2021-07-14 00:45:26 +0900

    숫자판점프

---
+++ 2주차/상근이의여행.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 상근이의여행 extends IOException {
+    public static void Main(String[] args){
+        
+
+    }
+    
+}
